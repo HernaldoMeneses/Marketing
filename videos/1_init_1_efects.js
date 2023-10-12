28a7e36d827ede5d6828f995dc250228e1b16a2b
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-12 16:04:46 -0300

    Update 1_init_1_efects.js

--- videos/1_init_1_efects.js
+++ videos/1_init_1_efects.js
@@ -1,21 +1,30 @@
-﻿// Criar uma nova composição (nome, altura, largura, ver, ver, ver);
-var comp = app.project.items.addComp("Minha Composição", 1920, 1080, 30, 10, 30);
+﻿// Criar uma nova composição (nome, largura, altura, ver, ver, ver);
+var comp = app.project.items.addComp("Minha Composição", 1080, 1920, 30, 10, 30);
 
 //Criar um objeto windows (ver , titulo, ver )
-var mainWindow = new Window("palette","Add Title an Cut" ,undefined);
+//var mainWindow = new Window("palette","Add Title an Cut" ,undefined);
 
-var groupOne = mainWindow.add("group", undefined, "groupOne");
+//var groupOne = mainWindow.add("group", undefined, "groupOne");
 //groupOnde.add("statictext", undefined,"This script allows you to add a title and cut you video");
-//var titleName = groupOnde.add("edittext", undefined, "Title of Video");
+
 
 // Adicionar uma camada de texto à composição
 var textLayer = comp.layers.addText("Olá, Mundo!");
 
+// Definir algumas propriedades do texto
+textLayer.position.setValue([comp.width / 2, comp.height / 2]);
+textLayer.scale.setValue([2000, 2000]);
+textLayer.opacity.setValue(100);
 
+
+// Definir o texto para ser em negrito
+var textDocument = textLayer.property("Source Text").value;
+textDocument.fontStyle == "Bold";
+textLayer.property("Source Text").setValue(textDocument);
 
 
 //Open window.
-mainWindow.show();
+//mainWindow.show();
 
 // Salvar o projeto
 app.project.save();
