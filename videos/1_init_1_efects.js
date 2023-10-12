1249b359f4fc85f07ffd4c685602e51dd429c952
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-12 15:55:47 -0300

    Update 1_init_1_efects.js

--- videos/1_init_1_efects.js
+++ videos/1_init_1_efects.js
@@ -1,10 +1,24 @@
-// Criar uma nova composição (nome, altura, largura, ver, ver, ver);
+﻿// Criar uma nova composição (nome, altura, largura, ver, ver, ver);
 var comp = app.project.items.addComp("Minha Composição", 1920, 1080, 30, 10, 30);
 
 //Criar um objeto windows (ver , titulo, ver )
 var mainWindow = new Window("palette","Add Title an Cut" ,undefined);
-mainWindow.show();
 
+var groupOne = mainWindow.add("group", undefined, "groupOne");
+//groupOnde.add("statictext", undefined,"This script allows you to add a title and cut you video");
+//var titleName = groupOnde.add("edittext", undefined, "Title of Video");
+
+// Adicionar uma camada de texto à composição
+var textLayer = comp.layers.addText("Olá, Mundo!");
+
+
+
+
+//Open window.
+mainWindow.show();
 
 // Salvar o projeto
 app.project.save();
+
+
+
