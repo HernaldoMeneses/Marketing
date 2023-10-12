2ca637f1f293a2bfccbcc5c379f9618d1c4ecc36
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-12 15:41:11 -0300

    Create 1_init_1_efects.js

---
+++ videos/1_init_1_efects.js
@@ -0,0 +1,7 @@
+// Criar uma nova composição
+var comp = app.project.items.addComp("Minha Composição", 1920, 1080, 30, 10, 30);
+
+var mainWindow = new Window("palette","Add Title an Cut" ,undefined);
+mainWindow.show();
+// Salvar o projeto
+//app.project.save();
