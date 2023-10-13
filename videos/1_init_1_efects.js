a79d437695e46c3dfc06938ad3051f504b8b9728
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-13 01:10:41 -0300

    Update 1_init_1_efects.js

--- videos/1_init_1_efects.js
+++ videos/1_init_1_efects.js
@@ -1,27 +1,25 @@
 ﻿// Criar uma nova composição (nome, largura, altura, ver, ver, ver);
 var comp = app.project.items.addComp("Minha Composição", 1080, 1920, 30, 10, 30);
 
-//Criar um objeto windows (ver , titulo, ver )
-//var mainWindow = new Window("palette","Add Title an Cut" ,undefined);
-
-//var groupOne = mainWindow.add("group", undefined, "groupOne");
-//groupOnde.add("statictext", undefined,"This script allows you to add a title and cut you video");
-
-
 // Adicionar uma camada de texto à composição
 var textLayer = comp.layers.addText("Olá, Mundo!");
 
 // Definir algumas propriedades do texto
 textLayer.position.setValue([comp.width / 2, comp.height / 2]);
-textLayer.scale.setValue([2000, 2000]);
+textLayer.scale.setValue([200, 200]);
 textLayer.opacity.setValue(100);
 
 
-// Definir o texto para ser em negrito
+var textLayer = app.project.activeItem.layer("Seu Texto Layer");
 var textDocument = textLayer.property("Source Text").value;
-textDocument.fontStyle == "Bold";
-textLayer.property("Source Text").setValue(textDocument);
 
+var novaCor = [1, 0, 0]; // Vermelho (R, G, B)
+textDocument.fillColor.setValue(novaCor);
+
+// Definir a opacidade da cor
+textDocument.fillColor.alpha = 1; // 1 para opaco, 0 para transparente
+
+textLayer.property("Source Text").setValue(textDocument);
 
 //Open window.
 //mainWindow.show();
