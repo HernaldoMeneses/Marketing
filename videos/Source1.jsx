e6adcddcb5f96879b226b7d4987f4eb56771d898
HernaldoMeneses <hernaldomeneses@gmail.com>
2023-10-13 01:50:36 -0300

    Create Source1.jsx

---
+++ videos/Source1.jsx
@@ -0,0 +1,23 @@
+﻿// Criar uma nova composição
+var comp = app.project.items.addComp("Minha Composição", 1920, 1080, 30, 10, 30);
+
+// Adicionar uma camada de texto à composição
+var textLayer = comp.layers.addText("Olá, Mundo!");
+
+// Definir algumas propriedades do texto
+textLayer.position.setValue([comp.width / 2, comp.height / 2]);
+textLayer.scale.setValue([200, 200]);
+textLayer.opacity.setValue(50);
+
+// Definir o texto para ser em negrito
+var textDocument = textLayer.property("Source Text").value;
+textDocument.fontStyle = "Bold";
+textLayer.property("Source Text").setValue(textDocument);
+
+// Definir a cor do texto para vermelho
+var textFillColor = textLayer.property("Source Text").value.fillColor;
+textFillColor.setValue([1, 0, 0, 1]); // (R, G, B, Opacidade)
+textLayer.property("Source Text").setValue("Olá, Mundo!", textFillColor);
+
+// Salvar o projeto
+app.project.save();
